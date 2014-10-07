990955801f11fc8464c697f657f40ce831e1a005
Ratik <Ratik96@gmail.com>
2014-10-08 01:03:22 +0530

    Converted onTouch() to an instance variable

--- GestureBrightnessListener.java
+++ GestureBrightnessListener.java
@@ -17,25 +17,28 @@ private void getWindowData (Context context) {
   maxHeight = windowManager.getDefaultDisplay().getHeight();
 }
 
-@Override
-public boolean onTouch(View v, MotionEvent event) {
-  switch(event.getActionMasked()){
-  case MotionEvent.ACTION_DOWN:
-    // Nothing here
-    break;
-  case MotionEvent.ACTION_UP:
-    // To suppress warnings
-    v.performClick();
-    break;
-  case MotionEvent.ACTION_MOVE:
-    rawYCoordinate = (int) event.getY();
-    yCoordinate = 255 - (rawYCoordinate / nFactor);
-    android.provider.Settings.System.putInt(mContext.getContentResolver(), 
-        android.provider.Settings.System.SCREEN_BRIGHTNESS, yCoordinate);
-    break;
+// Heart and soul of this implementation
+OnTouchListener gestureListener = new OnTouchListener() {
+  @Override
+  public boolean onTouch(View v, MotionEvent event) {
+    switch(event.getActionMasked()) {
+    case MotionEvent.ACTION_DOWN:
+      // Nothing here
+      break;
+    case MotionEvent.ACTION_UP:
+      // To suppress warnings
+      v.performClick();
+      break;
+    case MotionEvent.ACTION_MOVE:
+      rawYCoordinate = (int) event.getY();
+      yCoordinate = 255 - (rawYCoordinate / nFactor);
+      android.provider.Settings.System.putInt(mContext.getContentResolver(), 
+          android.provider.Settings.System.SCREEN_BRIGHTNESS, yCoordinate);
+      break;
+    }
+    return true;
   }
-  return true;
-}
+};
 
 .
 .
